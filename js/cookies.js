3fbdaeb8d0a65de4d5e0942f63f039f94e230c9f
Lucas Antonio Maggio <marsinformatica@gmail.com>
2020-10-24 14:00:38 +0200

    Cartel de Cookies listo, todo bastante terminado, faltan algunos detalles y empesar con el Blog

---
+++ js/cookies.js
@@ -0,0 +1,24 @@
+/*ésto comprueba la localStorage si ya tiene la variable guardada */
+function compruebaAceptaCookies() {
+
+    if (localStorage.aceptaCookies != 'true') {
+        cajacookies.style.display = 'flex';
+
+
+    }
+}
+
+/* aquí guardamos la variable de que se ha
+aceptado el uso de cookies así no mostraremos
+el mensaje de nuevo */
+function aceptarCookies() {
+    localStorage.aceptaCookies = 'true';
+    cajacookies.style.display = 'none';
+}
+
+/* ésto se ejecuta cuando la web está cargada */
+$(document).ready(function() {
+    compruebaAceptaCookies();
+
+
+});
